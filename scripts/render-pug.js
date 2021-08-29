abdeab28d0935a2c75a7b72b9dc4b93db44cd4c6
Liam Keegan <liam@keegan.ch>
2021-08-29 12:12:05 +0200

    don't prettify generated html

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const upath = require("upath");
 const pug = require("pug");
 const sh = require("shelljs");
-const prettier = require("prettier");
 
 module.exports = function renderPug(filePath) {
   const destPath = filePath
@@ -23,15 +22,5 @@ module.exports = function renderPug(filePath) {
     sh.mkdir("-p", destPathDirname);
   }
 
-  const prettified = prettier.format(html, {
-    printWidth: 1000,
-    tabWidth: 4,
-    singleQuote: true,
-    proseWrap: "preserve",
-    endOfLine: "lf",
-    parser: "html",
-    htmlWhitespaceSensitivity: "ignore",
-  });
-
-  fs.writeFileSync(destPath, prettified);
+  fs.writeFileSync(destPath, html);
 };
