542dd218d2dfe66b658e1f692ba7e2685ba2e1ca
Liam Keegan <liam@keegan.ch>
2021-09-03 12:34:19 +0200

    add missing js bootstrap include for responsive navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // webpack entry point
 
-// import Bootstrap JS
-import Carousel from "bootstrap/js/dist/carousel";
+// import selected Bootstrap JS
+
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+import "bootstrap/js/dist/carousel";
+import "bootstrap/js/dist/collapse";
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
 
 // import CSS
+
 import "./scss/styles.scss";
