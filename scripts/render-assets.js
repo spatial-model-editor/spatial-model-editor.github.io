1b78f125514434c407a54d2d3a7c1bae5a852fef
Liam Keegan <liam@keegan.ch>
2022-11-20 11:49:35 +0100

    replace libsquoosh with sharp
    - squoosh doesn't support node 18 (current LTS version) and is no longer in active development
    - replace with alternative library sharp
    - add node 18 CI job & use it for deployment

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -1,8 +1,7 @@
 "use strict";
-const fs = require("fs");
 const upath = require("upath");
 const sh = require("shelljs");
-const libSquoosh = require("@squoosh/lib");
+const sharp = require("sharp");
 
 module.exports = function renderAssets() {
   const sourcePath = upath.resolve(upath.dirname(__filename), "../src/assets");
@@ -11,52 +10,17 @@ module.exports = function renderAssets() {
   // copy original images
   sh.cp("-R", sourcePath, destPath);
 
-  // set low number of workers: default is nthreads, but gives asm out of memory error
-  const imagePool = new libSquoosh.ImagePool(4);
-
   // make webp versions of png images
-  _convertPngImages(destPath, imagePool).then(
-    console.log("Started asynchronous png -> webp conversion...")
-  );
-};
-
-async function _convertPngImages(destPath, imagePool) {
-  var images = [];
-  for (const filePath of sh.find(destPath)) {
-    if (filePath.match(/\.png$/)) {
-      images.push(imagePool.ingestImage(filePath));
+  for (const inFile of sh.find(destPath)) {
+    if (inFile.match(/\.png$/)) {
+      const outFile = inFile.replace(/\.png$/, ".webp");
+      sharp(inFile)
+        .toFile(outFile)
+        .then((info) => {
+          const relIn = upath.relative(destPath, inFile);
+          const relOut = upath.relative(destPath, outFile);
+          console.log(`- ${relIn} --> ${relOut}`);
+        });
     }
   }
-
-  await Promise.all(images.map((a) => a.decoded));
-
-  const encodeOptions = {
-    webp: {
-      lossless: true,
-      method: 6,
-    },
-  };
-
-  var encodes = [];
-  for (const image of images) {
-    encodes.push(image.encode(encodeOptions));
-  }
-  await Promise.all(encodes);
-
-  var outputs = [];
-  for (const image of images) {
-    const outFile = image.file.replace(/\.png$/, ".webp");
-    const rawData = (await image.encodedWith.webp).binary;
-    outputs.push(
-      fs.writeFile(outFile, rawData, (err) => {
-        if (err) throw err;
-        const relIn = upath.relative(destPath, image.file);
-        const relOut = upath.relative(destPath, outFile);
-        console.log(`- ${relIn} --> ${relOut}`);
-      })
-    );
-  }
-  await Promise.all(outputs);
-
-  return imagePool.close();
-}
+};
